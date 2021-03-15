ed2e411d997ce6f19fe369d09d2a466c856ea3cd
Marius <marius.pierenkemper@gmail.com>
2021-03-15 16:21:50 +0100

    labeltool: shows and lists fonts

--- labeltool/labeltool.py
+++ labeltool/labeltool.py
@@ -1,35 +1,49 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QListWidget, QLabel, QSlider
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
-from PIL import ImageFont, ImageDraw, Image
+from PIL import ImageFont, ImageDraw, Image, ImageQt
 from glob import glob
-import numpy as np
-
-sample_text = "Lorem ipsum dolor"
-im_size = (800,100)
+import os
 
 font_dir= 'C:/Users/Schnee/Datasets/Fonts01CleanUp/'
-
 fonts = glob(font_dir + '*.*', recursive=True)
-
-font = ImageFont.truetype(fonts[0], 20)
-im = Image.new('L', (im_size[0], im_size[1]), 0)
-draw = ImageDraw.Draw(im)
-w, h = draw.textsize(sample_text, font=font)
-draw.text(((im_size[0])/2, (im_size[1])/2), sample_text, font=font, fill='#FFF')
-
-im = np.array(im)/255.0*2.0 - 1.0
-im = np.expand_dims(im, axis=-1)
-
-#pg.image(im)
-
 
 app = QApplication([])
 window = QWidget()
 layout = QVBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-layout.addWidget(pg.image(im))
+label = QLabel()
+font_list = QListWidget()
+slider_1 = QSlider(Qt.Horizontal)
+slider_2 = QSlider(Qt.Horizontal)
+
+layout.addWidget(label)
+layout.addWidget(font_list)
+layout.addWidget(slider_1)
+layout.addWidget(slider_2)
+
+def select_font(font):
+    set_sample_text(font_dir + font.text())
+
+def set_sample_text(font, sample_text="The quick brown fox jumps over the lazy dog", im_size=(1000,100), txt_size=35):
+    font = ImageFont.truetype(font, txt_size)
+    im = Image.new('L', (im_size[0], im_size[1]), 0)
+    draw = ImageDraw.Draw(im)
+    w, h = draw.textsize(sample_text, font=font)
+    draw.text((im_size[0]/2-w/2, im_size[1]/2-h/2), sample_text, font=font, fill='#FFF')
+
+    qImg = ImageQt.ImageQt(im)
+    qPix = QPixmap.fromImage(qImg)
+    label.setPixmap(qPix)
+
+for font in fonts:
+    filename = os.path.basename(font)
+    font_list.addItem(filename)
+
+font_list.itemActivated.connect(select_font)
+
+set_sample_text(fonts[0])
+
 window.setLayout(layout)
 window.show()
 app.exec()
