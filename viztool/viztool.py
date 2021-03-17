344a23467f30c56a21999f5e284a2b45346a3339
Marius <marius.pierenkemper@gmail.com>
2021-03-17 22:17:15 +0100

    viztool load model + save image

--- viztool/viztool.py
+++ viztool/viztool.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QListWidget, QLabel, QSlider, QPushButton, QShortcut
-from PyQt5.QtGui import QPixmap, QKeySequence
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QWidget, QVBoxLayout, QGridLayout, QLabel, QSlider, QPushButton, QFileDialog
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
 import numpy as np
@@ -7,6 +7,8 @@ import tensorflow as tf
 from PIL import Image, ImageQt
 import sys
 from functools import partial
+import os
+from datetime import datetime
 
 class Viztool(QWidget):
 
@@ -55,17 +57,46 @@ class Viztool(QWidget):
             self.latent_slider[i].setMaximum(self.slider_steps)
             self.latent_slider[i].setMinimumHeight(200)
             self.latent_slider[i].valueChanged.connect(partial(self.slider_changed, i))
-            self.latent_slider_captions[i].setText(f'<b>{i}</b>')
+            self.latent_slider_captions[i].setText(f'<b>{i+1}</b>')
             self.layout_latent_slider.addWidget(self.latent_slider[i], 0 + 2 * (i // self.slider_per_row), i % self.slider_per_row)
             self.layout_latent_slider.addWidget(self.latent_slider_captions[i], 1 + 2 * (i // self.slider_per_row), i % self.slider_per_row)
 
+        self.layout_btns = QHBoxLayout()
+
         self.btn_random = QPushButton('Random')
         self.btn_random.clicked.connect(self.randomize_latent)
-        self.layout.addWidget(self.btn_random)
+        self.layout_btns.addWidget(self.btn_random)
+
+        self.btn_select_model = QPushButton('Select Model')
+        self.btn_select_model.clicked.connect(self.select_model)
+        self.layout_btns.addWidget(self.btn_select_model)
+
+        self.btn_save = QPushButton('Save as Image')
+        self.btn_save.clicked.connect(self.save)
+        self.layout_btns.addWidget(self.btn_save)
+
+        self.layout.addLayout(self.layout_btns)
 
         self.setWindowTitle('Viztool')
         self.setLayout(self.layout)
         self.show()
+
+    def save(self):
+        savedir = f'{self.modeldir}../viztool/'
+        if not os.path.exists(savedir):
+            os.makedirs(savedir)
+        filename = f'{datetime.now().strftime("%Y-%m-%d-%H%M%S")}.png'
+        self.output_img.save(f'{savedir}{filename}')
+
+    def select_model(self):
+        modeldir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        if modeldir:
+            model = tf.saved_model.load(self.modeldir)
+            if model:
+                self.model = model
+                self.modeldir = modeldir
+            else:
+                print("Wrong directory")
 
     def randomize_latent(self):
         self.input_latent = np.random.normal(0, 0.5, size=(1, self.latent_dim))
@@ -91,9 +122,10 @@ class Viztool(QWidget):
         for i in range(self.output.shape[0]):
             imgs.append(tf.keras.preprocessing.image.array_to_img(self.output[i]))
 
-        self.output_img = Image.new('L', (self.num_img*self.img_size, self.img_size))
+        num_rows = 2
+        self.output_img = Image.new('L', (self.num_img//num_rows*self.img_size, self.img_size*num_rows))
         for i in range(len(imgs)):
-            self.output_img.paste(imgs[i], (self.img_size*i, 0))
+            self.output_img.paste(imgs[i], (self.img_size*(i%(self.num_img//num_rows)), (i // (self.num_img//num_rows)) * self.img_size))
 
         img_height = int(self.output_img.size[1] * img_width / self.output_img.size[0])
 
