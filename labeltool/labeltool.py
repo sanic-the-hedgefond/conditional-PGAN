4ea74d1336af3bd203216c84f825ab50a2f8bd03
Marius <marius.pierenkemper@gmail.com>
2021-03-17 09:58:35 +0100

    cleanup labeltool

--- labeltool/labeltool.py
+++ labeltool/labeltool.py
@@ -7,167 +7,177 @@ from glob import glob
 import os
 import yaml
 from functools import partial
+import copy
+import sys
 
-font_dir = 'C:/Users/Schnee/Datasets/Fonts01CleanUp/'
-font_types = ['*.otf', '*.ttf']
+class Labeltool(QWidget):
 
-fonts = []
-for type in font_types:
-    fonts.extend(glob(font_dir + type))
+    def __init__(self):
+        super().__init__()
 
-label_file = '00_labels.yaml'
-current_row = 0
-current_font = os.path.basename(fonts[current_row])
+        label_file = '00_labels.yaml'
+        font_types = ['*.otf', '*.ttf']
+        self.font_dir = 'C:/Users/Schnee/Datasets/Fonts01CleanUp/'
 
-slider_steps = 20
+        self.fonts = []
+        for type in font_types:
+            self.fonts.extend(glob(self.font_dir + type))
 
-label_names = ['Weight', 'Width', 'Contrast', 'Serifs', 'Italic', 'Roundness']
-labels = dict()
+        self.current_row = 0
+        self.current_font = os.path.basename(self.fonts[self.current_row])
 
-label_cache = [0.0] * len(label_names)
+        self.slider_steps = 20
 
-if not os.path.exists(font_dir + label_file):
-    for font in fonts:
-        filename = os.path.basename(font)
-        labels[filename] = [0.0] * len(label_names)
+        self.label_names = ['Weight', 'Width', 'Contrast', 'Serifs', 'Italic', 'Roundness']
+        self.labels = dict()
 
-    with open(font_dir + label_file, 'w') as f:
-        yaml.dump(labels, f)
+        self.label_cache = [0.0] * len(self.label_names)
 
-else:
-    with open(font_dir + label_file) as f:
-        labels = yaml.load(f, Loader=yaml.FullLoader)
+        if not os.path.exists(self.font_dir + label_file):
+            for font in self.fonts:
+                filename = os.path.basename(font)
+                labels[filename] = [0.0] * len(self.label_names)
 
-app = QApplication([])
-window = QWidget()
+            with open(self.font_dir + label_file, 'w') as f:
+                yaml.dump(self.labels, f)
 
-layout = QHBoxLayout()
+        else:
+            with open(self.font_dir + label_file) as f:
+                self.labels = yaml.load(f, Loader=yaml.FullLoader)
 
-layout_left = QVBoxLayout()
-layout_right = QVBoxLayout()
+        self.initUI()
 
-layout.addLayout(layout_left)
-layout.addLayout(layout_right)
+    def initUI(self):
+        self.layout = QHBoxLayout()
 
-font_list = QListWidget()
-caption = QLabel()
-font_view = QLabel()
+        self.layout_left = QVBoxLayout()
+        self.layout_right = QVBoxLayout()
 
-btn_prev = QPushButton('Prev (Left Arrow Key)')
-btn_next = QPushButton('Next (Right Arrow Key)')
-btn_copy = QPushButton('Copy Labels (Strg+C)')
-btn_paste = QPushButton('Paste Labels (Strg+V))')
-btn_save = QPushButton('Save (Return)')
+        self.layout.addLayout(self.layout_left)
+        self.layout.addLayout(self.layout_right)
 
-slider_captions = [QLabel() for _ in range(len(label_names))]
-sliders = [QSlider(Qt.Horizontal) for _ in range(len(label_names))]
+        self.font_list = QListWidget()
+        self.caption = QLabel()
+        self.font_view = QLabel()
 
-layout_left.addWidget(font_list)
+        self.btn_prev = QPushButton('Prev (Left Arrow Key)')
+        self.btn_next = QPushButton('Next (Right Arrow Key)')
+        self.btn_copy = QPushButton('Copy Labels (Ctrl+C)')
+        self.btn_paste = QPushButton('Paste Labels (Ctrl+V)')
+        self.btn_save = QPushButton('Save (Ctrl+S)')
 
-layout_right.addWidget(caption)
-layout_right.addWidget(font_view)
+        self.slider_captions = [QLabel() for _ in range(len(self.label_names))]
+        self.sliders = [QSlider(Qt.Horizontal) for _ in range(len(self.label_names))]
 
-layout_sliders = QGridLayout()
+        self.layout_left.addWidget(self.font_list)
 
-for i in range(len(label_names)):
-    slider_captions[i].setText(label_names[i])
-    layout_sliders.addWidget(slider_captions[i], i, 0)
-    layout_sliders.addWidget(sliders[i], i, 1)
+        self.layout_right.addWidget(self.caption)
+        self.layout_right.addWidget(self.font_view)
 
-layout_right.addLayout(layout_sliders)
+        self.layout_sliders = QGridLayout()
 
-layout_btns = QHBoxLayout()
+        for i in range(len(self.label_names)):
+            self.slider_captions[i].setText(self.label_names[i])
+            self.sliders[i].setTickPosition(QSlider.TicksBelow)
+            self.sliders[i].setTickInterval(2)
+            self.layout_sliders.addWidget(self.slider_captions[i], i, 0)
+            self.layout_sliders.addWidget(self.sliders[i], i, 1)
 
-layout_btns.addWidget(btn_prev)
-layout_btns.addWidget(btn_next)
-layout_btns.addWidget(btn_copy)
-layout_btns.addWidget(btn_paste)
-layout_btns.addWidget(btn_save)
+        self.layout_right.addLayout(self.layout_sliders)
 
-layout_right.addLayout(layout_btns)
+        layout_btns = QHBoxLayout()
 
-def next_font():
-    global current_row
-    current_row = (current_row + 1) % (len(fonts))
-    font = font_list.item(current_row)
-    set_sample_text(font_dir + font.text())
+        layout_btns.addWidget(self.btn_prev)
+        layout_btns.addWidget(self.btn_next)
+        layout_btns.addWidget(self.btn_copy)
+        layout_btns.addWidget(self.btn_paste)
+        layout_btns.addWidget(self.btn_save)
 
-def prev_font():
-    global current_row
-    current_row = (current_row - 1) % (len(fonts))
-    font = font_list.item(current_row)
-    set_sample_text(font_dir + font.text())
+        self.layout_right.addLayout(layout_btns)
 
-def select_font(font):
-    global current_row
-    current_row = font_list.currentRow()
-    set_sample_text(font_dir + font.text())
+        for font in self.fonts:
+            filename = os.path.basename(font)
+            self.font_list.addItem(filename)
 
-def set_sample_text(font, sample_text="ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz .,-!?/", im_size=(1200,200), txt_size=55):
-    global current_font
-    current_font = os.path.basename(font)
-    update_caption()
-    font = ImageFont.truetype(font, txt_size)
-    im = Image.new('L', (im_size[0], im_size[1]), 0)
-    draw = ImageDraw.Draw(im)
-    _, h = draw.textsize(sample_text, font=font)
-    draw.text((20, im_size[1]/2-h/2), sample_text, font=font, fill='#FFF')
+        self.font_list.itemActivated.connect(self.select_font)
 
-    q_img = ImageQt.ImageQt(im)
-    q_pix = QPixmap.fromImage(q_img)
-    font_view.setPixmap(q_pix)
+        self.btn_next.clicked.connect(self.next_font)
+        self.btn_prev.clicked.connect(self.prev_font)
+        self.btn_copy.clicked.connect(self.copy_label)
+        self.btn_paste.clicked.connect(self.paste_label)
+        self.btn_save.clicked.connect(self.save_labels)
 
-    for i, slider in enumerate(sliders):
-        slider.setValue(labels[current_font][i] * slider_steps)
+        self.btn_next.setShortcut(QKeySequence('Right'))
+        self.btn_prev.setShortcut(QKeySequence('Left'))
+        self.btn_copy.setShortcut(QKeySequence('Ctrl+C'))
+        self.btn_paste.setShortcut(QKeySequence('Ctrl+V'))
+        self.btn_save.setShortcut(QKeySequence('Ctrl+S'))
 
-def slider_changed(i):
-    labels[current_font][i] = sliders[i].value() / slider_steps
-    update_caption()
+        for i in range(len(self.sliders)):
+            self.sliders[i].setMinimum(-self.slider_steps)
+            self.sliders[i].setMaximum(self.slider_steps)
+            self.sliders[i].valueChanged.connect(partial(self.slider_changed, i))
 
-def update_caption():
-    caption.setText(f'Font {current_row+1} of {len(fonts)}. Current font: {current_font}. Labels: {labels[current_font]}')
+        self.select_font(self.font_list.item(self.current_row))
 
-def save_labels():
-    with open(font_dir + label_file, 'w') as f:
-        yaml.dump(labels, f)
+        self.setWindowTitle('Labeltool')
+        self.setLayout(self.layout)
+        self.show()
 
-def copy_label():
-    global label_cache
-    label_cache = labels[current_font]
+    def next_font(self):
+        self.current_row = (self.current_row + 1) % (len(self.fonts))
+        font = self.font_list.item(self.current_row)
+        self.set_sample_text(self.font_dir + font.text())
 
-def paste_label():
-    global labels
-    global label_cache
-    labels[current_font] = label_cache
+    def prev_font(self):
+        self.current_row = (self.current_row - 1) % (len(self.fonts))
+        font = self.font_list.item(self.current_row)
+        self.set_sample_text(self.font_dir + font.text())
 
-    for i, slider in enumerate(sliders):
-        slider.setValue(labels[current_font][i] * slider_steps)
+    def select_font(self, font):
+        self.current_row = self.font_list.currentRow()
+        self.set_sample_text(self.font_dir + font.text())
 
-for font in fonts:
-    filename = os.path.basename(font)
-    font_list.addItem(filename)
+    def set_sample_text(self, font, sample_text="ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz .,-!?/", im_size=(1200,200), txt_size=55):
+        self.current_font = os.path.basename(font)
+        self.update_caption()
+        font = ImageFont.truetype(font, txt_size)
+        im = Image.new('L', (im_size[0], im_size[1]), 0)
+        draw = ImageDraw.Draw(im)
+        _, h = draw.textsize(sample_text, font=font)
+        draw.text((20, im_size[1]/2-h/2), sample_text, font=font, fill='#FFF')
 
-font_list.itemActivated.connect(select_font)
+        q_img = ImageQt.ImageQt(im)
+        q_pix = QPixmap.fromImage(q_img)
+        self.font_view.setPixmap(q_pix)
 
-btn_next.clicked.connect(next_font)
-btn_prev.clicked.connect(prev_font)
-btn_copy.clicked.connect(copy_label)
-btn_paste.clicked.connect(paste_label)
-btn_save.clicked.connect(save_labels)
+        for i, slider in enumerate(self.sliders):
+            slider.setValue(self.labels[self.current_font][i] * self.slider_steps)
 
-btn_next.setShortcut(QKeySequence('Right'))
-btn_prev.setShortcut(QKeySequence('Left'))
-btn_copy.setShortcut(QKeySequence('Ctrl+C'))
-btn_paste.setShortcut(QKeySequence('Ctrl+V'))
-btn_save.setShortcut(QKeySequence('Return'))
+    def slider_changed(self, i):
+        self.labels[self.current_font][i] = self.sliders[i].value() / self.slider_steps
+        self.update_caption()
 
-for i in range(len(sliders)):
-    sliders[i].setMinimum(-slider_steps)
-    sliders[i].setMaximum(slider_steps)
-    sliders[i].valueChanged.connect(partial(slider_changed, i))
+    def update_caption(self):
+        self.caption.setText(f'<b>Font {self.current_row+1} of {len(self.fonts)}. Current font: {self.current_font}. Labels: {self.labels[self.current_font]}</b>')
 
-select_font(font_list.item(current_row))
+    def save_labels(self):
+        with open(self.font_dir + label_file, 'w') as f:
+            yaml.dump(self.labels, f)
 
-window.setLayout(layout)
-window.show()
-app.exec()
+    def copy_label(self):
+        self.label_cache = copy.copy(self.labels[self.current_font])
+
+    def paste_label(self):
+        self.labels[self.current_font] = copy.copy(self.label_cache)
+
+        for i, slider in enumerate(self.sliders):
+            slider.setValue(self.labels[self.current_font][i] * self.slider_steps)
+
+def main():
+    app = QApplication([])
+    labeltool = Labeltool()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
